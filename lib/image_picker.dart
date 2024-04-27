156d530276d821d3570c3717253b927f782808b4
dhruvsuratriya <154353053+dhruvsuratriya@users.noreply.github.com>
2024-04-26 19:42:31 -0700

    image_picker.dart

--- lib/image_picker.dart
+++ lib/image_picker.dart
@@ -94,3 +94,137 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+
+
+// import 'dart:io';
+
+// import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
+
+// class ChatScreen extends StatefulWidget {
+//   @override
+//   _ChatScreenState createState() => _ChatScreenState();
+// }
+
+// class _ChatScreenState extends State<_ChatScreenState> {
+//   List<Message> _messages = [];
+
+//   final TextEditingController _textController = TextEditingController();
+
+//   late File _imageFile;
+
+//   Future<void> _getImage() async {
+//     final picker = ImagePicker();
+//     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+//     setState(() {
+//       if (pickedFile != null) {
+//         _imageFile = File(pickedFile.path);
+//       } else {
+//         print('No image selected.');
+//       }
+//     });
+//   }
+
+//   void _sendMessage() {
+//     if (_textController.text.isNotEmpty || _imageFile != null) {
+//       setState(() {
+//         _messages.add(
+//           Message(
+//             text: _textController.text,
+//             image: _imageFile,
+//             isMe: true,
+//           ),
+//         );
+//         _textController.clear();
+//         _imageFile;
+//       });
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Chat App'),
+//       ),
+//       body: Column(
+//         children: [
+//           Expanded(
+//             child: ListView.builder(
+//               itemCount: _messages.length,
+//               itemBuilder: (context, index) {
+//                 return _buildMessage(_messages[index]);
+//               },
+//             ),
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.all(8.0),
+//             child: Row(
+//               children: [
+//                 IconButton(
+//                   icon: Icon(Icons.photo),
+//                   onPressed: _getImage,
+//                 ),
+//                 Expanded(
+//                   child: TextField(
+//                     controller: _textController,
+//                     decoration: InputDecoration(
+//                       hintText: 'Enter your message...',
+//                     ),
+//                   ),
+//                 ),
+//                 IconButton(
+//                   icon: Icon(Icons.send),
+//                   onPressed: _sendMessage,
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+
+//   Widget _buildMessage(Message message) {
+//     return Row(
+//       mainAxisAlignment:
+//           message.isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+//       children: [
+//         if (!message.isMe)
+//           CircleAvatar(
+//             child: Text('User'),
+//           ),
+//         if (message.image != null)
+//           Image.file(
+//             message.image,
+//             width: 200,
+//             height: 200,
+//           ),
+//         if (message.text.isNotEmpty)
+//           Container(
+//             margin: EdgeInsets.all(8.0),
+//             padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+//             decoration: BoxDecoration(
+//               color: message.isMe ? Colors.blue : Colors.grey[300],
+//               borderRadius: BorderRadius.circular(8.0),
+//             ),
+//             child: Text(
+//               message.text,
+//               style:
+//                   TextStyle(color: message.isMe ? Colors.white : Colors.black),
+//             ),
+//           ),
+//       ],
+//     );
+//   }
+// }
+
+// class Message {
+//   final String text;
+//   final File image;
+//   final bool isMe;
+
+//   Message({required this.text, required this.image, required this.isMe});
+// }
